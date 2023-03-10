09e3e78312cb1ce2129b9da104a6ff04935e4289
LeOShI98 <68815673+LeOShI98@users.noreply.github.com>
2023-03-11 10:34:26 +1100

    Add dropout functionality to HiddenLayer.py

--- algorithms/HiddenLayer.py
+++ algorithms/HiddenLayer.py
@@ -12,17 +12,27 @@ from .Activation import Activation
 class HiddenLayer:
 
     def __init__(self,n_in, n_out,
-                 activation_last_layer='tanh',activation='tanh', W=None, b=None):
+                 activation_last_layer='tanh',activation='tanh',  W=None, b=None):
         '''
         Typical hidden layer of a MLP: units are fully-connected and have
         sigmoidal activation function. Weight matrix W is of shape (n_in,n_out)
         and the bias vector b is of shape (n_out,).
         '''
+        
         self.input=None
         self.activation=Activation(activation).f
+        self.drop = None
+        
+        # potentially redundent, we probably only need the activation type and check if == "softmax" then final layer, if not then hidden layer
+        # take less argument for instantiation. or we could just add a method called add_last_layer to handle the last layer      Leo
+        self.activation_type = activation
+        self.last_layer_act_type = activation_last_layer
         
         # activation deriv of last layer
         self.activation_deriv=None
+        
+        
+
         
         if activation_last_layer:
             self.activation_deriv=Activation(activation_last_layer).f_deriv
@@ -42,23 +52,55 @@ class HiddenLayer:
         # we set he size of weight gradation as the size of weight
         self.grad_W = np.zeros(self.W.shape)
         self.grad_b = np.zeros(self.b.shape)
+           
+           
+           
+           
+           
+           
+    def set_keep_prob(self, keep_prob):
+        self.drop = Dropout(keep_prob)
+           
+           
     
-    def forward(self, input):
+    def forward(self, input, mode):
         '''
         :type input: numpy.array
         :param input: a symbolic tensor of shape (n_in,)
         '''
+        
         lin_output = np.dot(input, self.W) + self.b
+        
+        # if self.activation_type == self.last_layer_act_type:
+        # check if this is the last layer by checking the activation type
+        # could be written as 
+        # if self.activation_type == "softmax"
+        # do batch norm and dropout here
+        
+        
+
         self.output = (
             lin_output if self.activation is None
             else self.activation(lin_output)
         )
+           
+        # dropout after activation, need to set a keep probability with set_keep_prob, for final layer, should be 0
+        self.output = self.drop.forward(self.output, mode)
+           
         self.input=input
         return self.output
     
     def backward(self, delta, output_layer=False):         
         self.grad_W = np.atleast_2d(self.input).T.dot(np.atleast_2d(delta))
         self.grad_b = delta
+           
+           
+           
+        # run drop out to backprob   
+        delta = self.drop.backward(delta)   
+           
+           
+           
         if self.activation_deriv:
             delta = delta.dot(self.W.T) * self.activation_deriv(self.input)
         return delta
