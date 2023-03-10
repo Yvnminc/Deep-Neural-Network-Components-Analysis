15d051b61f5f757f781d60e4bade6004b2ffbaf5
LeOShI98 <68815673+LeOShI98@users.noreply.github.com>
2023-03-10 19:23:18 +1100

    Create Dropout.py
    Need to be imported and incorporated to the HiddenLayer Module and Mlp Module

---
+++ algorithms/Dropout.py
@@ -0,0 +1,47 @@
+import numpy as np
+
+class Dropout:
+    """
+    Description: The Dropout class allows performing regularization on the activation result through droping some nodes in a layer during training.
+    
+    Paramters:
+    - keep_prob: the probability of a node not being dropped
+    
+    Method:
+    - forward(A, mode): takes a activation result for one layer and apply a dropout mask on it so some of the activation becomes 0, this only happens when mode = "train"
+    - backward(dA): apply back prop on the layer that has went through the dropout process
+    
+    """
+    def __init__(self, keep_prob):
+        self.keep_prob = keep_prob
+        self.mask = None
+
+
+
+    def forward(self, A, mode):
+      
+        # According to the mode, perform dropout and return the activation 
+        
+        if mode == "train":
+            # Mask has the same shape as A
+            self.mask = np.random.rand(A.shape[0], A.shape[1])
+            # Turn the random number in mask into 0 and 1 
+            self.mask = (self.mask < self.keep_prob)
+            # Perfrom the element-wise multiplication and drop out some nodes in A
+            A = A * self.mask
+            # scale the remaing node up by the keep probability
+            A = A / self.keep_prob
+            return  A
+          
+        else:
+            # do not need to do drop out when testing
+            return  A
+
+
+    def backward(self, dA):
+      
+        # multiply by the same drop out binary matrix in the forward pass
+        dA = dA * self.mask 
+        # scale by the keep probability
+        dA = dA / self.keep_prob
+        return dA
